348c1d22040557c3a4d53210ce86faadc5bfef8c
figtereiscoding <quererqabv@gmail.com>
2023-08-30 16:04:40 +0300

    Create P04BasicQueueOperations.java

---
+++ A1StacksAndQueues/Exercise/P04BasicQueueOperations.java
@@ -0,0 +1,38 @@
+package A1StacksAndQueues.Exercise;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class P04BasicQueueOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int n = numbers[0];
+        int s = numbers[1];
+        int x = numbers[2];
+
+        ArrayDeque<Integer> numbersStack = new ArrayDeque<>();
+
+        Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .forEach(numbersStack::offer);
+
+        for (int i = 0; i < s; i++) {
+            numbersStack.poll();
+        }
+
+        if (numbersStack.isEmpty()) {
+            System.out.println(0);
+        } else if (numbersStack.contains(x)) {
+            System.out.println(true);
+        } else {
+            System.out.println(Collections.min(numbersStack));
+        }
+    }
+}
