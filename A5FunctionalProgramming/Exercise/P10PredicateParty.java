4a888578868a7b923394df1c6c4c1de3e14b1866
figtereiscoding <quererqabv@gmail.com>
2023-09-14 00:11:42 +0300

    Create P10PredicateParty.java

---
+++ A5FunctionalProgramming/Exercise/P10PredicateParty.java
@@ -0,0 +1,60 @@
+package A5FunctionalProgramming.Exercise;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class P10PredicateParty {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<String> guests = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .collect(Collectors.toList());
+
+        String command = scanner.nextLine();
+
+        while (!command.equals("Party!")) {
+            Predicate<String> predicate = null;
+
+            String criteria = command.split("\\s+")[2];
+
+            if (command.contains("Length")) {
+                predicate = x -> x.length() == Integer.parseInt(criteria);
+            } else if (command.contains("StartsWith")) {
+                predicate = x -> x.startsWith(criteria);
+            } else {
+                predicate = x -> x.endsWith(criteria);
+            }
+
+            if (command.contains("Remove")) {
+                ArrayList<String> toRemove = new ArrayList<>();
+                for (String guest : guests) {
+                    if (predicate.test(guest)) {
+                        toRemove.add(guest);
+                    }
+                }
+
+                guests.removeAll(toRemove);
+            } else {
+                ArrayList<String> toAdd = new ArrayList<>();
+                for (String guest : guests) {
+                    if (predicate.test(guest)) {
+                        toAdd.add(guest);
+                    }
+                }
+                guests.addAll(toAdd);
+            }
+
+            command = scanner.nextLine();
+        }
+
+        if(guests.isEmpty()){
+            System.out.println("Nobody is going to the party!");
+        }else{
+            System.out.println(guests.stream().sorted().collect(Collectors.toCollection(ArrayList::new)).toString().replaceAll("([\\[\\]])", "") + " are going to the party!");
+        }
+    }
+}
