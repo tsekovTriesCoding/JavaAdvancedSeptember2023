5a40588e400411a5f8e469372fbec1545891b65c
figtereiscoding <quererqabv@gmail.com>
2023-09-21 12:16:00 +0300

    Update P09PopulationCounter.java

--- A3SetsAndMapsAdvanced/Exercise/P09PopulationCounter.java
+++ A3SetsAndMapsAdvanced/Exercise/P09PopulationCounter.java
@@ -1,6 +1,7 @@
 package A3SetsAndMapsAdvanced.Exercise;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class P09PopulationCounter {
 
@@ -17,13 +18,44 @@ public class P09PopulationCounter {
             String country = tokens[1];
             long population = Long.parseLong(tokens[2]);
 
-            countriesPopulation.putIfAbsent(country,new LinkedHashMap<>());
-            countriesPopulation.get(country).putIfAbsent(city, population);
-
-            countries.put(country,population);
+            if (!countriesPopulation.containsKey(country)) {
+                countriesPopulation.put(country, new LinkedHashMap<>());
+                countriesPopulation.get(country).put(city, population);
+                countries.put(country, population);
+            } else {
+                countriesPopulation.get(country).put(city, population);
+                countries.put(country, countries.get(country) + population);
+            }
 
             input = scanner.nextLine();
         }
+
+        countriesPopulation.entrySet().stream().sorted((e1, e2) -> {
+                    long pop1 = 0;
+                    for (Long value : e1.getValue().values()) {
+                        pop1 += value;
+                    }
+                    long pop2 = 0;
+                    for (Long value : e2.getValue().values()) {
+                        pop2 += value;
+                    }
+                    return Long.compare(pop2, pop1);
+                })
+                .forEach(entry -> {
+                    System.out.print(entry.getKey() + " ");
+
+                    StringBuilder builder = new StringBuilder();
+                    AtomicLong totalPopulation = new AtomicLong();
+                    countriesPopulation.get(entry.getKey()).entrySet().stream().sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
+                            .forEach(e -> {
+                                builder.append(String.format("=>%s: %d%n", e.getKey(), e.getValue()));
+                                totalPopulation.addAndGet(e.getValue());
+                            });
+
+                    System.out.printf("(total population: %s)%n", totalPopulation);
+                    System.out.print(builder);
+                });
+
 
     }
 }
