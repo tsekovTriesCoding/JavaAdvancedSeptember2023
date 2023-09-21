ad0950122d332ed13ad150e5d98dd82a41d899ed
figtereiscoding <quererqabv@gmail.com>
2023-09-21 12:47:20 +0300

    added solution to problem 8

--- A6DefiningClasses/Exercise/P08FamilyTree/Main.java
+++ A6DefiningClasses/Exercise/P08FamilyTree/Main.java
@@ -1,18 +1,16 @@
 package A6DefiningClasses.Exercise.P08FamilyTree;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String personToFindInfoAbout = scanner.nextLine();
         String input = scanner.nextLine();
-        String infoAbout = input;
-
-        input = scanner.nextLine();
-        Map<String, Person> people = new HashMap<>();
+        LinkedHashMap<String, List<String>> childrenByParents = new LinkedHashMap<>();
+        List<Person> peopleList = new ArrayList<>();
+        Set<String> inputSet = new LinkedHashSet<>();
 
         while (!input.equals("End")) {
             if (input.contains("-")) {
@@ -20,7 +18,10 @@ public class Main {
                 String parentData = tokens[0];
                 String childData = tokens[1];
 
-
+                childrenByParents.putIfAbsent(parentData, new ArrayList<>());
+                childrenByParents.get(parentData).add(childData);
+                inputSet.add(parentData);
+                inputSet.add(childData);
             } else {
                 String personFirstName = input.split("\\s+")[0];
                 String personLastName = input.split("\\s+")[1];
@@ -28,10 +29,41 @@ public class Main {
                 String fullName = personFirstName + " " + personLastName;
 
                 Person person = new Person(fullName, personBirthdate);
-                people.put(fullName, person);
+                peopleList.add(person);
             }
 
             input = scanner.nextLine();
         }
+
+        for (String inputInfo : inputSet) {
+            Person currentPerson = getPersonInfo(inputInfo, peopleList);
+
+            for (Map.Entry<String, List<String>> entry : childrenByParents.entrySet()) {
+                Person parent = getPersonInfo(entry.getKey(), peopleList);
+
+                for (String childInfo : entry.getValue()) {
+                    Person child = getPersonInfo(childInfo, peopleList);
+
+                    if (currentPerson == child) {
+                        currentPerson.addToParentList(parent);
+                    } else if (currentPerson == parent) {
+                        currentPerson.addToChildrenList(child);
+                    }
+                }
+            }
+
+        }
+
+        System.out.println(getPersonInfo(personToFindInfoAbout, peopleList).toString());
+    }
+
+    private static Person getPersonInfo(String info, List<Person> personList) {
+        Person currentPerson = null;
+        for (Person person : personList) {
+            if (person.getName().equals(info) || person.getBirthdate().equals(info)) {
+                currentPerson = person;
+            }
+        }
+        return currentPerson;
     }
 }
--- A6DefiningClasses/Exercise/P08FamilyTree/Person.java
+++ A6DefiningClasses/Exercise/P08FamilyTree/Person.java
@@ -16,4 +16,49 @@ public class Person {
         this.children = new ArrayList<>();
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+
+    public void addToParentList(Person person) {
+        if (isNew(person, parents)) {
+            this.parents.add(person);
+        }
+    }
+
+    public void addToChildrenList(Person person) {
+        if (isNew(person, children)) {
+            this.children.add(person);
+        }
+    }
+
+    public boolean isNew(Person person, List<Person> personList) {
+        for (Person p : personList) {
+            if (p.equals(person)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String getOutputForList(List<Person> list) {
+        StringBuilder output = new StringBuilder();
+
+        if (!list.isEmpty()) {
+            for (Person p : list) {
+                output.append(String.format("%s %s%n", p.getName(), p.getBirthdate()));
+            }
+        }
+        return output.toString();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s%nParents:%n%sChildren:%n%s", name, String.join("/", this.birthdate), getOutputForList(parents), getOutputForList(children));
+    }
 }
