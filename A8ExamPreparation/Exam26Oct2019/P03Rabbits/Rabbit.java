9f903a66e301d9a7ae9cfef0c888515c2f9701db
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-10-19 19:24:55 +0300

    Create Rabbit.java

---
+++ A8ExamPreparation/Exam26Oct2019/P03Rabbits/Rabbit.java
@@ -0,0 +1,34 @@
+package A8ExamPreparation.Exam26Oct2019.P03Rabbits;
+
+public class Rabbit {
+    private String name;
+    private String species;
+    private boolean available;
+
+    public Rabbit(String name, String species) {
+        this.name = name;
+        this.species = species;
+        this.available = true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Rabbit (%s): %s", this.species, this.name);
+    }
+}
