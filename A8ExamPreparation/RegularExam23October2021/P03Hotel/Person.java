64361ae441aeef8c8fbe85964551da48b7d92d5c
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-10-20 20:57:18 +0300

    Create Person.java

---
+++ A8ExamPreparation/RegularExam23October2021/P03Hotel/Person.java
@@ -0,0 +1,36 @@
+package A8ExamPreparation.RegularExam23October2021.P03Hotel;
+
+public class Person {
+    private String name;
+    private int id;
+    private int age;
+    private String hometown = "n/a";
+
+    public Person(String name, int id, int age, String hometown) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.hometown = hometown;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Person %s: %d, Age: %d, Hometown: %s", this.name, this.id, this.age, this.hometown);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+}
