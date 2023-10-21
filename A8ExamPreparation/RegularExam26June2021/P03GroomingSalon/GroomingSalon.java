91460575a0e7ad6167a5d777691193ff93bc62e3
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2023-10-21 20:19:23 +0300

    added solution to third problem

---
+++ A8ExamPreparation/RegularExam26June2021/P03GroomingSalon/GroomingSalon.java
@@ -0,0 +1,53 @@
+package A8ExamPreparation.RegularExam26June2021.P03GroomingSalon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroomingSalon {
+    private int capacity;
+    private List<Pet> data;
+
+    public GroomingSalon(int capacity) {
+        this.capacity = capacity;
+        this.data = new ArrayList<>();
+    }
+
+    public void add(Pet pet) {
+        if (this.data.size() < this.capacity) {
+            this.data.add(pet);
+        }
+    }
+
+    public boolean remove(String name) {
+        for (Pet pet : this.data) {
+            if (pet.getName().equals(name)) {
+                this.data.remove(pet);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Pet getPet(String name, String owner) {
+        for (Pet pet : this.data) {
+            if (pet.getName().equals(name) && pet.getOwner().equals(owner)) {
+                return pet;
+            }
+        }
+
+        return null;
+    }
+
+    public int getCount() {
+        return this.data.size();
+    }
+
+    public String getStatistics() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("The grooming salon has the following clients:").append(System.lineSeparator());
+        this.data.forEach(pet -> sb.append(pet.getName()).append(" ").append(pet.getOwner()).append(System.lineSeparator()));
+
+        return sb.toString();
+    }
+}
